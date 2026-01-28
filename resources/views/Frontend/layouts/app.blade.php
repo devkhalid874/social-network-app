bc80b05948ae85cb905f0f9d680c585d1424e442
Khalid Majeed <kmajeed85400@gmail.com>
2026-01-28 21:51:30 +0500

    wednesday updated code

--- resources/views/Frontend/layouts/app.blade.php
+++ resources/views/Frontend/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="icon" type="image/png" href="{{asset('assets/images/logo-16x16.png')}}" />
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Argon - Social Network</title>
+    <title>Social App</title>
 
      <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Major+Mono+Display" rel="stylesheet">
